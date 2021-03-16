f7b0a5e7f80b151ff504a88d4ebc52c637e23757
maximfedoretswebdev <69167344+maximfedoretswebdev@users.noreply.github.com>
2021-03-15 20:14:52 -0700

    Add files via upload
    Practice redesign, HAS 3 Pages

---
+++ Jaascript.js
@@ -0,0 +1,48 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL WELCOME*/
+sr.reveal('.welcome_pic',{}); 
+sr.reveal('.welcome_text',{}); 
+
+
+/*SCROLL ABOUT*/
+sr.reveal('.about_text',{}); 
+sr.reveal('.about_pic',{}); 
+
+/*Hours */
+sr.reveal('.hours', {});
+
+
+
+/* Accepting*/ 
+sr.reveal('.accepting_header',{});
+sr.reveal('.accepting_text',{});
+sr.reveal('.btn-dark',{});
+
+
+/*Footer*/
+sr.reveal('.footer_top', {});
+sr.reveal('.footer_bottom', {});
+
+
+/*Gallery*/
+sr.reveal('.column', {});
+
+
+/*Contact*/
+sr.reveal('.contact_header', {});
+sr.reveal('.card-text', {});
+sr.reveal('.btn-dark', {});
+sr.reveal('.card-body',{});
+
+
+
+
+
+
+
